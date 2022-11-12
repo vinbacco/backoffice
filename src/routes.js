a3b07eee302798c65eb6cfc13d632a600a5bf9c3
marco.allotta <marco.allotta@cws.it>
2022-11-12 19:45:08 +0100

    addi category detail to study json form

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,12 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'));
 const ToursList = React.lazy(() => import('./modules/tours/views/ToursList'));
 const ToursDetail = React.lazy(() => import('./modules/tours/views/ToursDetail'));
 const NotFound = React.lazy(() => import('./views/pages/page404/Page404'));
-const ProductCategoriesList = React.lazy(() => import('./modules/productCategories/views/ProductCategoriesList'));
 const ProductCategoryDetail = React.lazy(() => import('./modules/productCategories/views/ProductCategoryDetail'));
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/tours', name: 'Tours', element: ToursList },
   { path: '/tour/:id', name: 'Tour', element: ToursDetail },
-  { path: '/categories', name: 'Categorie', element: ProductCategoriesList },
   { path: '/categories/:id', name: 'CategorieDettaglio', element: ProductCategoryDetail },
   { path: '*', name: 'NotFound', element: NotFound },
 ];
